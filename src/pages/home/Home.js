22dba100c2500a6258860e826be4975335c4cddb
Noah do Rego <noah.dorego@gmail.com>
2022-07-21 20:56:48 -0400

    Added onClick to next arrow to change page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
+import { Container, Typography } from '@mui/material';
 
 import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 import FileUploadRoundedIcon from '@mui/icons-material/FileUploadRounded';
@@ -11,10 +12,16 @@ import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded';
 import background from "../../images/megafy_background1.png"
 
 import styles from './styles';
-import { Container, Typography } from '@mui/material';
+
+import { useHistory } from "react-router-dom";
 
 function Home() {
   const classes = styles;
+  let history = useHistory();
+
+  const nextPage = () => {
+    history.push("/upload");
+  }
 
   return (
     <div style={{ backgroundImage: `url(${background})`, overflow: "hidden" }}>
@@ -57,7 +64,7 @@ function Home() {
           </Box>
         </Container>
 
-        <ArrowForwardIosRoundedIcon sx={classes.next}></ArrowForwardIosRoundedIcon>
+        <ArrowForwardIosRoundedIcon sx={classes.next} onClick={nextPage()}></ArrowForwardIosRoundedIcon>
         
       </Box>
 
